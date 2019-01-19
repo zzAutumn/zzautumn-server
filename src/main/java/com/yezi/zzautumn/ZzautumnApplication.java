fed59cb5ed51e9f13e750613df084118e5456fa2
yezi <jingjing.ye@dragonsoftbravo.com>
2019-01-19 21:23:26 +0800

    update article

--- src/main/java/com/yezi/zzautumn/ZzautumnApplication.java
+++ src/main/java/com/yezi/zzautumn/ZzautumnApplication.java
@@ -2,8 +2,6 @@ package com.yezi.zzautumn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
-import org.springframework.context.annotation.Bean;
 
 import java.util.TimeZone;
 
@@ -12,12 +10,6 @@ public class ZzautumnApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ZzautumnApplication.class, args);
-    }
-
-    @Bean
-    public Jackson2ObjectMapperBuilderCustomizer jacksonObjectMapperCustomization() {
-        return jacksonObjectMapperBuilder ->
-                jacksonObjectMapperBuilder.timeZone(TimeZone.getDefault());
     }
 
 }
